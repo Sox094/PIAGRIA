63b3a79b580aa7c9637befa42421d912efa18c3b
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-11-13 17:16:32 +0000

    Creation Controller Form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users,
                :path => '', 
                :path_names => {:sign_in => 'login', :sign_out => 'logout', :edit => 'profile'}
+  
+  
   root 'pages#home'
+  
+  resources :rooms
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
