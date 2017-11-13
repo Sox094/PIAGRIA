4793d2edac13ee5c9b8d6a824d04ba9dd214c99d
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-11-13 16:53:02 +0000

    Creation Model Form

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113143622) do
+ActiveRecord::Schema.define(version: 20171113164930) do
+
+  create_table "forms", force: :cascade do |t|
+    t.string   "nom"
+    t.string   "description"
+    t.date     "datedecreation"
+    t.integer  "user_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "forms", ["user_id"], name: "index_forms_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
