1a9c9cc1a06aac1fca82b7b9d0edc40d19c618a4
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-11-17 23:18:50 +0000

    Modifs de quelques classes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116134012) do
+ActiveRecord::Schema.define(version: 20171117211603) do
 
   create_table "forms", force: :cascade do |t|
     t.string   "nom"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20171116134012) do
   end
 
   add_index "forms", ["user_id"], name: "index_forms_on_user_id"
+
+  create_table "formulaires", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
