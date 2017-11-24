3ebed7f38408ad1d9d611897e1177d18fafd1ebf
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2017-11-24 11:33:33 +0000

    Suppression MVC forms

--- app/models/form.rb
+++
@@ -1,9 +0,0 @@
-class Form < ActiveRecord::Base
-  belongs_to :user
-  
-  validates :nom, presence: true
-  validates :description, presence: true
-  validates :datedecreation, presence: true
-
-
-end
