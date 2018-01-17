7c38f4c20970589b9a04e4d93391979825572d9b
Hakim GHANEM <hakim.ghanem@telecomnancy.net>
2018-01-17 08:58:04 +0000

    Modifs model question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,7 @@
 class Question < ActiveRecord::Base
   belongs_to :formulaire
-  has_many :answers, dependent: :destroy
-  accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:nom].blank? }, :allow_destroy => true
+  has_many :answers, :dependent => :destroy
+  accepts_nested_attributes_for :answers, :allow_destroy => true
   
   #self.inheritance_column = nil
   enum typequestion: [:reponse_courte, :choix_multiple, :choix_simple]
