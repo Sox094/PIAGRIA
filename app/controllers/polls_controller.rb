b544a171e128100e0156bf7f74933dc4982b8719
Sofian CHOUDER <sofian.chouder@telecomnancy.net>
2018-01-30 14:20:57 +0000

    Forms répondable

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -8,14 +8,9 @@ class PollsController < ApplicationController
         @formulaire = Formulaire.find(params[:formulaire_id])
         @polls = @formulaire.polls.order("created_at DESC")
         @polls = Poll.where(formulaire_id: @formulaire)
-        
-        @polls2 = @polls.group("nom") 
-        
         @answers = Answer.where(formulaire_id: @formulaire_id)
         @selecto = 3
         #@answers = Answer.where(Answer.id = @polls.answer_id)
-
-    
 
     end
     
@@ -34,13 +29,26 @@ class PollsController < ApplicationController
 
 def create
     @formulaire = Formulaire.find(params[:formulaire_id])
-    
     @formulaire.questions.each_with_index do |question, i|
+            question.answers.each_with_index do |answer, j|
                 if !params[:"nom_#{i}"].blank?
+                    puts "MY PARAM: #{params["nom_#{i}"]}\n"
+                    if answer.id.to_s == "#{params["nom_#{i}"]}"
+                        Poll.create formulaire_id: (params[:formulaire_id]), question_id: (Answer.find(params["nom_#{i}"]).question_id), answer_id: (params["nom_#{i}"]), nom: (Answer.find(params["nom_#{i}"]).content)
+                        #On fait passer l'answer id dans la table poll et on récupère à partir de cet answer id le nom et la question_id
+                    end
+                    puts "apres if"
+                elsif !params["nom_#{i}_#{j}"].blank?
                     puts "MY PARAM: #{params["nom_#{i}_#{j}"]}\n"
-                    Poll.create formulaire_id: (params[:formulaire_id]), question_id: (Answer.find(params["nom_#{i}"]).question_id), answer_id: (params["nom_#{i}"]), nom: (Answer.find(params["nom_#{i}"]).content)
+                    Poll.create formulaire_id: (params[:formulaire_id]), question_id: (Answer.find(params["nom_#{i}_#{j}"]).question_id), answer_id: (params["nom_#{i}_#{j}"]), nom: (Answer.find(params["nom_#{i}_#{j}"]).content)
                     #On fait passer l'answer id dans la table poll et on récupère à partir de cet answer id le nom et la question_id
                 end
+            end
+            if !params[:"champs_#{i}"].blank?
+                    puts "MY PARAM: #{params["champs_#{i}"]}\n"
+                    Poll.create formulaire_id: (params[:formulaire_id]), question_id: (question.id), answer_id: (Answer.maximum(:id).next), nom: (params["champs_#{i}"])
+                    Answer.create formulaire_id: (params[:formulaire_id]), question_id: (question.id), content: (params["champs_#{i}"])
+            end
     end
     
       @question = @formulaire.questions
